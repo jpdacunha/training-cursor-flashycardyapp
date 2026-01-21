a6c1b939f05792c247f7ebd9a2d94f895dbe615c
jpdacunha <ser@XPS15JP.localdomain>
2026-01-21 10:27:48 +0100

    4 cards per line display

--- app/[locale]/decks/[deckId]/deck-detail-client.tsx
+++ app/[locale]/decks/[deckId]/deck-detail-client.tsx
@@ -147,7 +147,7 @@ export function DeckDetailClient({ deck, cards: initialCards }: DeckDetailClient
             </CardContent>
           </Card>
         ) : (
-          <div className="space-y-4">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
             {cards.map((card, index) => (
               <Card key={card.id}>
                 <CardHeader>
